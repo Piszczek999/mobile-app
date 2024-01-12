cd1bdd093da1987a1ac3f40d0a63940ef78d8ba5
Piszczek <ognisty001@gmail.com>
2024-01-12 23:07:04 +0100

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [content, setContent] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.navbar}>
+        <Button title="Profile" onPress={() => setContent("Profile")} />
+        <Button title="Misje" onPress={() => setContent("Misje")} />
+        <Button title="Sklep" onPress={() => setContent("Sklep")} />
+        <Button title="Ranking" onPress={() => setContent("Ranking")} />
+      </View>
+      <Text style={styles.textColor.white}>{content}</Text>
     </View>
   );
 }
@@ -13,8 +21,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#365486",
+  },
+  navbar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: "#0F1035",
+    maxHeight: 50,
+  },
+  textColor: {
+    white: {
+      color: "white",
+    },
+    black: {
+      color: "black",
+    },
   },
 });
